a38bda419c57e9ad305a229cf1b10d17c64e6ee3
Paul Tagliamonte <tag@pault.ag>
2014-09-21 00:04:56 -0400

    insert

--- dockerfdw/wrappers/containers.py
+++ dockerfdw/wrappers/containers.py
@@ -39,6 +39,47 @@ class ContainerFdw(BaseDockerFdw):
         self.client.stop(id_, timeout=10)
         self.client.remove_container(id_)
 
+    def insert(self, new_values):
+        raise NotImplementedError("INSERT not implemented yet")
+
+        required = ['image']
+        blacklist = ['bridge', 'ip', 'pid', 'exit_code', 'id',
+                     'running', 'names']
+        default = {'privileged': False,
+                   'command': None,
+                   'name': None,}
+        config = {}
+
+        for el in blacklist:
+            if el in new_values:
+                if new_values[el] is not None:
+                    raise ValueError("Error: Can not handle column `%s`" % (el))
+                new_values.pop(el)
+
+        for el in required:
+            if el not in new_values:
+                raise ValueError("Required column `%s' missing." % (el))
+            config[el] = new_values.pop(el)
+
+        for el in default:
+            if el in new_values:
+                config[el] = new_values.pop(el)
+            else:
+                config[el] = default[el]
+
+        self.client.create_container(detach=False, **config)
+
+        #    image, command=None, hostname=None, user=None,
+        #    detach=False, stdin_open=False, tty=False, mem_limit=0,
+        #    ports=None, environment=None, dns=None, volumes=None,
+        #    volumes_from=None, network_disabled=False, name=None,
+        #    entrypoint=None, cpu_shares=None, working_dir=None,
+        #    memswap_limit=0
+        #)
+
+
+        assert new_values == {}
+
     def execute(self, quals, columns):
         for container in self.proxy.list():
             yield {x: nully(self.spec[x](self.proxy.get(container['Id'])))
